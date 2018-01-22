07fbcd2c5e34c14c447ffc984b91843a499adaa5
magnusohrstrom <magnusohrstrom330@gmail.com>
2018-01-22 09:53:06 +0100

    Last commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,9 @@
-
 const toggleClassesOnMenuClick = () => {
     $('#menu-icon').click(()=>{
         $('#menu-icon').toggleClass('open');
         $('#navigation').toggleClass('is-open');
         $('#projects-li').removeClass('is-open');
         $('#search-form').removeClass('is-open');
-        
-        //$('#navigation').hasClass('is-open') ? toggleClassOnElement('#navigation','is-closed'):'';
     });
 }
 
@@ -25,9 +22,19 @@ const toggleClassesOnSearchIconClick = () => {
     });
 }
 
+const smoothScrollToAHref = () => {
+    $('a[href*=\\#]').on('click', function(event) {
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
+}
+
+
 //initiates click functions on document ready.
 $(document).ready(()=>{
     toggleClassesOnMenuClick();
     toggleClassesOnSearchIconClick();
     toggleClassOnSubMenuClick();
+    smoothScrollToAHref();
 });
+
